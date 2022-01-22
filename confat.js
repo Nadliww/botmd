7aa90a882eee37a7252347864006b6409f609845
Nadliww <79284219+Nadliww@users.noreply.github.com>
2022-01-22 23:12:57 +0700

    Update confat.js

--- confat.js
+++ confat.js
@@ -3,10 +3,10 @@ const chalk = require('chalk')
 const setting = JSON.parse(fs.readFileSync('./setting.json'));
 
 // Other
-global.owner = ["6285342106390","62887435047326","62887435047326"]
-global.packname = '© Alphabot MD'
+global.owner = ["6285342106390","62895701081582","62895701081582"]
+global.packname = '© Haruka'
 global.youtube = 'https://youtube.com/c/ZeeoneOfc'
-global.author = 'ZeeoneOfc'
+global.author = 'Liww'
 global.sessionName = 'session'
 global.flaming = "https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text="
 global.prefa = ['','!','.','🐦','🐤','🗿']
